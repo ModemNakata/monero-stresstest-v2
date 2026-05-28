3b543d584f035a593de93856126bafd15c230ed6
Nakata Modem <nakatamodem@hotmail.com>
2026-05-28 06:45:38 +0200

    shell scripts

---
+++ node.sh
@@ -0,0 +1,1 @@
+./monerod --config-file=monerod.conf
---
+++ rpc.sh
@@ -0,0 +1,2 @@
+./monero-wallet-rpc --rpc-bind-port=28088 --rpc-bind-ip=127.0.0.1 --confirm-external-bind --wallet-file=monero-storage/wallets/testnet-stressnet/testnet-stressnet --disable-rpc-login --daemon-address=127.0.0.1:28089 --testnet --password=123 --log-level=1 
+
